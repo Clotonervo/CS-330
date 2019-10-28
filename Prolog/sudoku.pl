0b33a76a9203af4e58e44c0db1625e95498c2d3c
Clotonervo <sam.hopkins@blastsoccer.net>
2019-10-27 21:52:12 -0600

    Started Sudoku

--- Prolog/sudoku.pl
+++ Prolog/sudoku.pl
@@ -1,14 +1,14 @@
 % A Sudoku solver.  The basic idea is for each position,
 % check that it is a digit with `digit`.  Then verify that the digit
-% chosen doesn't violate any constraints (row, column, and cube).
+% chosen doesnt violate any constraints (row, column, and cube).
 % If no constraints were violated, proceed further.  If a constraint
 % was violated, then backtrack to the last digit choice and move from
 % there (the Prolog engine should handle this for you automatically).
 % If we reach the end of the board with this scheme, it means that
 % the whole thing is solved.
- 
+
 % YOU SHOULD FILL IN THE SOLVE PROCEDURE, DOWN BELOW.
- 
+
 digit(1).
 digit(2).
 digit(3).
@@ -18,11 +18,13 @@ digit(6).
 digit(7).
 digit(8).
 digit(9).
- 
+
+:- use_module(library(lists)).
+
 numBetween(Num, Lower, Upper) :-
         Num >= Lower,
         Num =< Upper.
- 
+
 % cubeBounds: (RowLow, RowHigh, ColLow, ColHigh, CubeNumber)
 cubeBounds(0, 2, 0, 2, 0).
 cubeBounds(0, 2, 3, 5, 1).
@@ -33,7 +35,7 @@ cubeBounds(3, 5, 6, 8, 5).
 cubeBounds(6, 8, 0, 2, 6).
 cubeBounds(6, 8, 3, 5, 7).
 cubeBounds(6, 8, 6, 8, 8).
- 
+
 % Given a board and the index of a column of interest (0-indexed),
 % returns the contents of the column as a list.
 % columnAsList: (Board, ColumnNumber, AsRow)
@@ -41,7 +43,7 @@ columnAsList([], _, []).
 columnAsList([Head|Tail], ColumnNum, [Item|Rest]) :-
         nth0(ColumnNum, Head, Item),
         columnAsList(Tail, ColumnNum, Rest).
- 
+
 % given which row and column we are in, gets which cube
 % is relevant.  A helper ultimately for `getCube`.
 % cubeNum: (RowNum, ColNum, WhichCube)
@@ -49,7 +51,7 @@ cubeNum(RowNum, ColNum, WhichCube) :-
         cubeBounds(RowLow, RowHigh, ColLow, ColHigh, WhichCube),
         numBetween(RowNum, RowLow, RowHigh),
         numBetween(ColNum, ColLow, ColHigh).
- 
+
 % Drops the first N elements from a list.  A helper ultimately
 % for `getCube`.
 % drop: (InputList, NumToDrop, ResultList)
@@ -59,7 +61,7 @@ drop([_|Tail], Num, Rest) :-
         Num > 0,
         NewNum is Num - 1,
         drop(Tail, NewNum, Rest).
- 
+
 % Takes the first N elements from a list.  A helper ultimately
 % for `getCube`.
 % take: (InputList, NumToTake, ResultList)
@@ -69,7 +71,7 @@ take([Head|Tail], Num, [Head|Rest]) :-
         Num > 0,
         NewNum is Num - 1,
         take(Tail, NewNum, Rest).
- 
+
 % Gets a sublist of a list in the same order, inclusive.
 % A helper for `getCube`.
 % sublist: (List, Start, End, NewList)
@@ -77,7 +79,7 @@ sublist(List, Start, End, NewList) :-
         drop(List, Start, TempList),
         NewEnd is End - Start + 1,
         take(TempList, NewEnd, NewList).
- 
+
 % Given a board and cube number, gets the corresponding cube as a list.
 % Cubes are 3x3 portions, numbered from the top left to the bottom right,
 % starting from 0.  For example, they would be numbered like so:
@@ -95,25 +97,50 @@ getCube(Board, Number, AsList) :-
         sublist(Row3, ColLow, ColHigh, Row3Nums),
         append(Row1Nums, Row2Nums, TempRow),
         append(TempRow, Row3Nums, AsList).
- 
+
 % Given a board, solve it in-place.
 % After calling `solve` on a board, the board should be fully
 % instantiated with a satisfying Sudoku solution.
- 
-% ---- PUT CODE HERE ---
-% ---- PUT CODE HERE ---
- 
+
+checkSquare(Square, Row, Col, Cube) :- (nonvar(Square); var(Square), digit(Square), is_set(Row), is_set(Col), is_set(Cube)).
+
+
+
 solve(Board) :- true.  % ---PUT CODE HERE---
- 
-% ---- PUT CODE HERE ---
-% ---- PUT CODE HERE ---
- 
+
+	columnAsList(Board, 0, Col_0),
+	columnAsList(Board, 1, Col_1),
+	columnAsList(Board, 2, Col_2),
+	columnAsList(Board, 3, Col_3),
+	columnAsList(Board, 4, Col_4),
+	columnAsList(Board, 5, Col_5),
+	columnAsList(Board, 6, Col_6),
+	columnAsList(Board, 7, Col_7),
+	columnAsList(Board, 8, Col_8),
+	Cols = [Col_0, Col_1, Col_2, Col_3, Col_4, Col_5, Col_6, Col_7, Col_8],
+
+	getCube(Board, 0, Cube_0),
+	getCube(Board, 1, Cube_1),
+	getCube(Board, 2, Cube_2),
+	getCube(Board, 3, Cube_3),
+	getCube(Board, 4, Cube_4),
+	getCube(Board, 5, Cube_5),
+	getCube(Board, 6, Cube_6),
+	getCube(Board, 7, Cube_7),
+	getCube(Board, 8, Cube_8),
+	Cubes = [Cube_0, Cube_1, Cube_2, Cube_3, Cube_4, Cube_5, Cube_6, Cube_7, Cube_8],
+
+	Rows = Board.
+
+
+
+
 % Prints out the given board.
 printBoard([]).
 printBoard([Head|Tail]) :-
         write(Head), nl,
         printBoard(Tail).
- 
+
 test1(Board) :-
         Board = [[2, _, _, _, 8, 7, _, 5, _],
                  [_, _, _, _, 3, 4, 9, _, 2],
@@ -126,7 +153,7 @@ test1(Board) :-
                  [_, 1, _, 8, 2, _, _, _, 5]],
         solve(Board),
         printBoard(Board).
- 
+
 test2(Board) :-
         Board = [[_, _, _, 7, 9, _, 8, _, _],
                  [_, _, _, _, _, 4, 3, _, 7],
