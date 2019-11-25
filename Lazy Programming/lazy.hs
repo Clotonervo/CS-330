82410146035c92436d002a15e660f5b3df17edf8
Clotonervo <sam.hopkins@blastsoccer.net>
2019-11-25 13:18:41 -0700

    lcsLength still needs work

--- Lazy Programming/lazy.hs
+++ Lazy Programming/lazy.hs
@@ -22,5 +22,13 @@ isPrimeFast n = [x | x <- takeWhile(<= iSqrt n) primesFast, n `mod` x == 0] == [
 primesFast :: [Int]
 primesFast = filter isPrimeFast [2.. ]
 
--- Type Signature
+-- lcsLength ********************************************
 lcsLength :: String -> String -> Int
+lcsLength str1 str2 = a  where
+  str1_length = length str1
+  str2_length = length str2
+                   a = array ((1,1),(n,n))
+                        ([((1,j), 1) | j <- [1..n]] ++
+                         [((i,1), 1) | i <- [2..n]] ++
+                         [((i,j), a!(i,j-1) + a!(i-1,j-1) + a!(i-1,j))
+                                     | i <- [2..n], j <- [2..n]])
